a43ca953d41613aa5128d3417e14e40d54a4a8b9
NallabellyVinay <52525335+NallabellyVinay@users.noreply.github.com>
2024-07-25 16:59:25 +0530

    Update alerter.py

--- alerter.py
+++ alerter.py
@@ -21,6 +21,6 @@ def alert_in_celcius(farenheit):
 
 alert_in_celcius(400.5)
 alert_in_celcius(303.6)
-aler_in_celcius(200)
+alert_in_celcius(200)
 print(f'{alert_failure_count} alerts failed.')
 print('All is well (maybe!)')
